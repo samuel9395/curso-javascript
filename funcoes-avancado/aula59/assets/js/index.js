74b7f244bd7b8156ccad8549bafd8c1cdc765517
Samuel Bitencourt <88355342+samuel9395@users.noreply.github.com>
2025-08-13 22:54:20 -0300

    Aula 59
    Nesta aula desenvolvemos uma calculadora utilizando a função construtora. Capturamos eventos e utilizamos também o método "this".

--- funcoes-avancado/aula59/assets/js/index.js
+++ funcoes-avancado/aula59/assets/js/index.js
@@ -1,6 +1,18 @@
 function Calculadora() {
     this.display = document.querySelector('.display');
 
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    }
+
+    this.capturaEnter = () => {
+        document.addEventListener('keypress', e => {
+            if (e.keyCode !== 13) return;
+            this.realizaConta();
+        })
+    }
+
     this.capturaCliques = () => {
         document.addEventListener('click', event => {
             const elemento = event.target;
@@ -11,20 +23,27 @@ function Calculadora() {
         });
     };
 
-    this.addNumDisplay = elemento => this.display.value += elemento.innerText;
+    this.addNumDisplay = elemento => {
+        this.display.value += elemento.innerText;
+        this.display.focus();
+    }
     this.clear = () => this.display.value = '';
     this.del = () => this.display.value = this.display.value.slice(0, -1);
     this.realizaConta = () => {
         try {
-            
+            const conta = eval(this.display.value);
+
+            if (!conta) {
+                alert('Conta inválida!');
+                return;
+            }
+
+            this.display.value = conta;
         } catch (e) {
-            alert('Conta inválida!')
+            alert('Conta inválida!');
+            return;
         }
     }
-    
-
-
-    this.inicia = () => this.capturaCliques();
 
 }
 
